81aad1e976f73638b816559945d8914544f97491
Yvon Meunier <meunieryvon6@gmail.com>
2021-11-01 15:47:46 -0400

    Finally it works!!!

--- src/com/company/RogueliteGame.java
+++ src/com/company/RogueliteGame.java
@@ -8,7 +8,7 @@ public class RogueliteGame {
     public void start() {
 
         floorGenerator = new FloorGenerator();
-        currentFloor = floorGenerator.generateFloor(9,13,5);// level doesnt generate bugs but width does
+        currentFloor = floorGenerator.generateFloor(50,50,80);// level doesnt generate bugs but width does
 
     }
 

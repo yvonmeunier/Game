fa46b9c6e430dff5755c2cf57fd5f9361ba8ae7a
Yvon Meunier <meunieryvon6@gmail.com>
2021-11-23 13:38:28 -0500

    aaaaaaaaa

--- out/production/Game/Default.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="default" tilewidth="52" tileheight="52" tilecount="324" columns="36">
- <image source="PC_Computer_-_The_Binding_of_Isaac_Rebirth_-_Necropolis.png" width="1899" height="468"/>
-</tileset>
